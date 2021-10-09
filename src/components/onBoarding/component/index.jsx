50b12f76450289f4c1002ed263b5ff4de09cc372
Jörg Kandziora <s55814@beuth-hochschule.de>
2021-10-09 10:57:19 +0200

    createt onboarding component

---
+++ src/components/onBoarding/component/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const OnBoarding = () => {
+
+  return (
+    <Container className="content-page">
+      <h1>Editor On Boarding</h1>
+      <h2>Add Shows</h2>
+      <p>
+
+
+      </p>
+      <h2>Accept or refuse Shows</h2>
+
+      <h2>Edit Shows</h2>
+
+      
+
+
+    </Container>
+  )
+
+}
+
+export default OnBoarding    
